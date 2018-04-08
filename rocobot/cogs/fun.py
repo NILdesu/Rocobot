dabe87a4b2079086b856f76d57b255f5f844b9dd
Wall-Dough <awalden0808@gmail.com>
2018-04-07 20:42:53 -0500

    Add spot command

--- rocobot/cogs/fun.py
+++ rocobot/cogs/fun.py
@@ -5,10 +5,22 @@ class Fun:
     def __init__(self, bot):
         self.bot = bot
 
+        self.rocos = []
+        with open('data/roco-cards.json', 'r') as file:
+            read_data = file.read()
+            self.rocos = json.loads(read_data)
+
+        random.seed()
+
     # shabadaba: yup
     @commands.command(name='shabadaba', description='Exactly what it sounds like')
     async def shabadaba(self, ctx):
         await ctx.send('(⋈•̀ᴗ•́⋈)')
+
+    @commands.command(name='spot', description='spot a roco')
+    async def spot_a_roco(self, ctx):
+        roco = self.rocos[random.randint(0, len(self.rocos))]
+        await ctx.send(f'Roco spotted:\n{roco}')
 
 
 def setup(bot):
