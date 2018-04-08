f8b13700c417da3fd179c35cc90f399f0fbc786c
NILdesu <zhiren_lim@yahoo.com>
2018-04-07 21:53:58 -0500

    embedded spot output

--- rocobot/cogs/fun.py
+++ rocobot/cogs/fun.py
@@ -22,8 +22,10 @@ class Fun:
     @commands.command(name='spot', description='spot a roco')
     async def spot_a_roco(self, ctx):
         roco = self.rocos[random.randint(0, len(self.rocos))]
-        await ctx.send(f'Roco spotted:\n{roco}')
 
+        embed = discord.Embed(title='Roco Spotted!', color=discord.Colour.gold())
+        embed.set_image(url=roco)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
