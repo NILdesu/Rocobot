57a01ea60c2b495c3a215801ade4428056d8f552
Wall-Dough <awalden0808@gmail.com>
2018-04-09 19:14:32 -0500

    Improve function naming and setup data management

--- rocobot/util/shows.py
+++ rocobot/util/shows.py
@@ -1,4 +1,16 @@
-async def help(ctx):
+SHOWS_DATA_LOCATION = 'data/shows.json'
+
+import util
+from util import io
+
+def _retrieve_shows_data():
+    print('retrieve_shows_data')
+    return {}
+
+def _store_shows_data(data):
+    print('store_shows_data')
+
+async def _shows_help(ctx):
     message = '```'
     message += 'add title\n'
     message += 'remove title\n'
@@ -11,28 +23,28 @@ async def help(ctx):
     message += '```'
     await ctx.send(message)
 
-async def add(ctx, title):
+async def _shows_add(shows, ctx, title):
     await ctx.send(f'add {title}')
 
-async def remove(ctx, title):
+async def _shows_remove(shows, ctx, title):
     await ctx.send(f'remove {title}')
 
-async def join(ctx, title):
+async def _shows_join(shows, ctx, title):
     await ctx.send(f'join {title}')
 
-async def leave(ctx, title):
+async def _shows_leave(shows, ctx, title):
     await ctx.send(f'leave {title}')
 
-async def invite(ctx, user, title):
+async def _shows_invite(shows, ctx, user, title):
     await ctx.send(f'invite {user} {title}')
 
-async def kick(ctx, user, title):
+async def _shows_kick(shows, ctx, user, title):
     await ctx.send(f'kick {user} {title}')
 
-async def recommend(ctx):
+async def _shows_recommend(shows, ctx):
     await ctx.send(f'recommend')
 
-async def list(ctx):
+async def _shows_list(shows, ctx):
     await ctx.send(f'list')
 
 async def handle_command(ctx, args):
@@ -45,47 +57,52 @@ async def handle_command(ctx, args):
 
     subcommand = args[0]
 
+
     if subcommand == 'help':
-        await help(ctx)
-    elif subcommand == 'add':
+        await _shows_help(ctx)
+        return
+
+    shows = _retrieve_shows_data()
+
+    if subcommand == 'add':
         if len(args) < 2:
             await invalid_syntax()
             return
         title = args[1]
-        await add(ctx, title)
+        await _shows_add(shows, ctx, title)
     elif subcommand == 'remove':
         if len(args) < 2:
             await invalid_syntax()
             return
         title = args[1]
-        await remove(ctx, title)
+        await _shows_remove(shows, ctx, title)
     elif subcommand == 'join':
         if len(args) < 2:
             await invalid_syntax()
             return
         title = args[1]
-        await join(ctx, title)
+        await _shows_join(shows, ctx, title)
     elif subcommand == 'leave':
         if len(args) < 2:
             await invalid_syntax()
             return
         title = args[1]
-        await leave(ctx, title)
+        await _shows_leave(shows, ctx, title)
     elif subcommand == 'invite':
         if len(args) < 3:
             await invalid_syntax()
             return
         user = args[1]
         title = args[2]
-        await invite(ctx, user, title)
+        await _shows_invite(shows, ctx, user, title)
     elif subcommand == 'kick':
         if len(args) < 3:
             await invalid_syntax()
             return
         user = args[1]
         title = args[2]
-        await kick(ctx, user, title)
+        await _shows_kick(shows, ctx, user, title)
     elif subcommand == 'recommend':
-        await recommend(ctx)
+        await _shows_recommend(shows, ctx)
     elif subcommand == 'list':
-        await list(ctx)
+        await _shows_list(shows, ctx)
