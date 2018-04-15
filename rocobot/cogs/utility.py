b009a566a486050ef1206fba0c4b0de2956886a0
Wall-Dough <awalden0808@gmail.com>
2018-04-15 11:21:24 -0500

    Add comment to show help message

--- rocobot/cogs/utility.py
+++ rocobot/cogs/utility.py
@@ -10,6 +10,25 @@ class Utility:
 
     @commands.command(name='shows')
     async def shows(self, ctx, *args):
+        """ A handy show tracker. Keeps track of shows and who is watching them.
+
+        Subcommands:
+        &shows add [show]
+        - Adds [show] to the list of shows
+        &shows remove [show]
+        - Removes [show] from the list of shows
+        &shows join [show]
+        - Adds you (the current user) as a watcher of [show]
+        &shows leave [show]
+        - Removes you (the current user) from the list of watchers of [show]
+        &shows invite [user] [show]
+        - Adds [user] as a watcher of [show]
+        &shows kick [user] [show]
+        - Removes [user] from the list of watchers of [show]
+        &shows list
+        - Lists all current shows in the list
+        &shows recommend
+        - Recommends a show to watch based on who is currently online"""
         await util.shows.handle_command(ctx, args)
 
 def setup(bot):
