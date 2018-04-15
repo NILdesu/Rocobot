c73fb0d6924b0c283d45aef32baa440fe2330e91
Wall-Dough <awalden0808@gmail.com>
2018-04-15 12:03:53 -0500

    Improve success/failure responses

--- rocobot/util/shows.py
+++ rocobot/util/shows.py
@@ -3,21 +3,19 @@ SHOWS_DATA_LOCATION = 'data/shows.json'
 import util
 from util import io
 
-async def _show_already_added(ctx):
-    await ctx.send('That show has already been added')
+async def _show_already_added(ctx, title):
+    await ctx.send(f'"{title}" has already been added to the list of shows.')
 
-async def _show_not_added(ctx):
-    await ctx.send('That show has not yet been added')
+async def _show_not_added(ctx, title):
+    await ctx.send(f'"{title}" has not yet been added to the list of shows.')
 
-async def _user_not_joined(ctx):
-    await ctx.send('This user has not yet joined the show')
+async def _user_not_joined(ctx, name, title):
+    await ctx.send(f'{name} is not watching "{title}."')
 
-async def _user_already_joined(ctx):
-    await ctx.send('This user has already joined the show')
+async def _user_already_joined(ctx, name, title):
+    await ctx.send(f'{name} is already watching "{title}."')
 
 def _retrieve_shows_data():
-    print('retrieve_shows_data')
-
     data = util.io.read_json(SHOWS_DATA_LOCATION)
 
     if (data == None):
@@ -30,63 +28,61 @@ def _store_shows_data(data):
 
 async def _shows_add(shows, ctx, title):
     if title in shows:
-        await _show_already_added(ctx)
+        await _show_already_added(ctx, title)
         return
 
     shows[title] = {}
     shows[title]['watchers'] = []
 
-    await ctx.send(f'add {title}')
+    await ctx.send(f'Added "{title}" to the list of shows.')
 
     _store_shows_data(shows)
 
 async def _shows_remove(shows, ctx, title):
     if title not in shows:
-        await _show_not_added(ctx)
+        await _show_not_added(ctx, title)
         return
 
     del shows[title]
 
-    await ctx.send(f'remove {title}')
+    await ctx.send(f'Removed "{title}" from the list of shows.')
 
     _store_shows_data(shows)
 
 async def _shows_invite(shows, ctx, name, title):
     if title not in shows:
-        await _show_not_added(ctx)
+        await _show_not_added(ctx, title)
         return
 
     if name in shows[title]['watchers']:
-        await _user_already_joined(ctx)
+        await _user_already_joined(ctx, name, title)
         return
 
     shows[title]['watchers'].append(name)
 
-    await ctx.send(f'invite {name} {title}')
+    await ctx.send(f'{name} is now watching "{title}."')
+
     _store_shows_data(shows)
 
 async def _shows_join(shows, ctx, title):
-    await ctx.send(f'join {title}')
-
     await _shows_invite(shows, ctx, f'<@{ctx.message.author.id}>', title)
 
 async def _shows_kick(shows, ctx, name, title):
     if title not in shows:
-        await _show_not_added(ctx)
+        await _show_not_added(ctx, title)
         return
 
     if name not in shows[title]['watchers']:
-        await _user_not_joined(ctx)
+        await _user_not_joined(ctx, name, title)
         return
 
     shows[title]['watchers'].remove(name)
 
-    await ctx.send(f'kick {name} {title}')
+    await ctx.send(f'{name} is no longer watching "{title}."')
 
     _store_shows_data(shows)
 
 async def _shows_leave(shows, ctx, title):
-    await ctx.send(f'leave {title}')
     await _shows_kick(shows, ctx, f'<@{ctx.message.author.id}>', title)
 
 async def _shows_recommend(shows, ctx):
